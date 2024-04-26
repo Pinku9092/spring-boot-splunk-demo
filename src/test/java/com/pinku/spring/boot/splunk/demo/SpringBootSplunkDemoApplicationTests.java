a4a64312810cecd9e4dc0dcc5571bfd6049c804e
Pinku <kpinku32@gmail.com>
2024-04-26 13:48:12 +0530

    first commit

---
+++ src/test/java/com/pinku/spring/boot/splunk/demo/SpringBootSplunkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinku.spring.boot.splunk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSplunkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
